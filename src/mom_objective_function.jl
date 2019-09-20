397853f9d78767b4ce312b86e0803f6c94eff572
Michael Laptop <michael.j.g.za@gmail.com>
2019-09-20 15:02:50 +0200

    fixed misspelling

--- src/mom_objective_function.jl
+++ src/mom_objective_function.jl
@@ -6,10 +6,10 @@ end
 function (mom::MoM)(simulated_log_price_paths::Array{Float64,2})
     log_price_stats = select_moments(mom.log_prices)
 
-    num_replications = size(simualted_log_prices, 2)
+    num_replications = size(simulated_log_price_paths, 2)
     G = zeros(num_replications, 5)
 
-    summary_stats_mat = get_summary_stats(simualted_log_prices, mom.log_prices)
+    summary_stats_mat = get_summary_stats(simulated_log_price_paths, mom.log_prices)
     G = summary_stats_mat - transpose(repeat(log_price_stats,1,num_replications))
     G = transpose(mean(G, dims = 1))
     return (transpose(G) * mom.weight_matrix * G)[1,1]
